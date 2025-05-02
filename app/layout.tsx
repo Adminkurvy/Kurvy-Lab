a5c510bd675363378f7a4acd423c2c4da706fa9c
Adminkurvy <admin@kurvylab.com>
2025-05-02 16:33:12 +0800

    Landing page
    Update for landing page

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'KurvyLab Wellness',
+  description: 'Kesihatan dalaman wanita',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
